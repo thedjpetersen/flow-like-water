2d219284eed8a9ada0a3ad1f7c19f6ac7d67a7af
DJ Petersen <thedjpetersen@gmail.com>
2023-12-07 12:45:42 -0800

    Add JSDoc annotations

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,47 @@ export type TaskState = "not_started" | "completed" | "in_process" | "failed";
 type TaskId = string;
 type TaskGroupId = string;
 
+/**
+ * Represents a task with execution logic, retry capability, and state management.
+ *
+ * A `Task` object encapsulates the logic for executing a task, a condition to check
+ * before execution, and mechanisms for retrying the task with specified intervals.
+ * It also maintains the task's current state and supports specifying subsequent tasks.
+ *
+ * @param {Object} options - Configuration options for the task.
+ * @param {TaskId} options.id - A unique identifier for the task.
+ * @param {Function} options.execute - A function that encapsulates the task's execution logic.
+ *                                     Should return a Promise that resolves to `void`, `TaskId`, or `TaskGroupId`.
+ * @param {Function} options.checkCondition - A function that returns a Promise resolving to a boolean,
+ *                                            indicating whether the task is ready to be executed.
+ * @param {TaskId[]} [options.nextTasks] - An optional array of task identifiers for tasks to be executed after this task.
+ * @param {number} [options.retries=0] - The number of times to retry the task if it fails. Defaults to 0.
+ * @param {number} [options.waitTime=1000] - The time in milliseconds to wait before retrying the task. Defaults to 1000ms.
+ *
+ * @property {TaskId} id - The unique identifier of the task.
+ * @property {Function} execute - The execution logic of the task.
+ * @property {Function} checkCondition - The function to check the precondition for the task execution.
+ * @property {number} retries - The number of retries for the task.
+ * @property {number} waitTime - The waiting time before a retry.
+ * @property {TaskId[]} [nextTasks] - The identifiers of subsequent tasks.
+ * @property {TaskState} [state] - The current state of the task, e.g., 'not_started', 'completed'.
+ * @property {number} [time] - The time taken by the task, updated after execution.
+ *
+ * @example
+ * // Creating a new Task
+ * const myTask = new Task({
+ *   id: 'task1',
+ *   execute: async () => {
+ *     // Task execution logic
+ *   },
+ *   checkCondition: async () => {
+ *     // Condition to check before execution
+ *     return true;
+ *   },
+ *   retries: 3,
+ *   waitTime: 2000
+ * });
+ */
 export class Task {
   id: TaskId;
   execute: () => Promise<void | TaskId | TaskGroupId>;
@@ -34,7 +75,27 @@ export class Task {
     this.time = 0;
   }
 
-  // Public only because of testing
+  /**
+   * Executes the task and manages its state and execution time.
+   *
+   * This method sets the task's state to 'in_process', executes the task's logic, and checks
+   * a post-execution condition. If the condition is not met, it throws an error. Otherwise,
+   * it calculates the execution time, updates the task's state to 'completed', and returns the result.
+   * In case of an error during execution or condition check, it sets the task's state to 'failed'
+   * and rethrows the error. This method is public primarily to facilitate testing.
+   *
+   * @returns {Promise<string | void>} A promise that resolves with the result of the task execution.
+   *                                   The result can be a string, or void if there is no return value.
+   * @throws {Error} Throws an error if the task execution fails or the post-execution condition is not met.
+   *
+   * @example
+   * // Running a task
+   * myTask.runTask().then(result => {
+   *   console.log('Task completed with result:', result);
+   * }).catch(error => {
+   *   console.error('Task failed:', error);
+   * });
+   */
   public runTask = async (): Promise<string | void> => {
     try {
       this.state = "in_process";
@@ -57,6 +118,28 @@ export class Task {
     }
   };
 
+  /**
+   * Executes the task with retry logic.
+   *
+   * This method attempts to run the task using `runTask`. If `runTask` throws an error,
+   * the method retries the execution based on the `retries` property of the task.
+   * Between each retry, it waits for a duration specified by the `waitTime` property.
+   * If the task continues to fail after all retries are exhausted, the error is thrown.
+   * The method ensures at least one execution of the task regardless of the retry count.
+   *
+   * @returns {Promise<string | void>} A promise that resolves with the result of the task execution,
+   *                                   or void if there is no return value. If the task fails even after
+   *                                   all retries, the promise is rejected with the encountered error.
+   * @throws {Error} Throws the error encountered during task execution if all retries fail.
+   *
+   * @example
+   * // Running a task with retry logic
+   * myTask.run().then(result => {
+   *   console.log('Task completed successfully:', result);
+   * }).catch(error => {
+   *   console.error('Task failed after retries:', error);
+   * });
+   */
   public run = async (): Promise<string | void> => {
     let currentRetries = 0;
     let hasRun = false;
@@ -89,24 +172,59 @@ type SerializedState = {
   };
 };
 
+/**
+ * Represents a group of tasks and/or task groups.
+ *
+ * This class provides a structure for organizing tasks and task groups under a single
+ * identifier. It facilitates managing related tasks collectively, allowing for
+ * the addition and removal of tasks or task groups from the collection.
+ *
+ * @property {TaskGroupId} id - The unique identifier of the task group.
+ * @property {Map<TaskId | TaskGroupId, Task | TaskGroup>} children - A map of child tasks and task groups.
+ */
 export class TaskGroup {
   id: TaskGroupId;
   children: Map<TaskId | TaskGroupId, Task | TaskGroup>;
 
+  /**
+   * Constructs a new instance of TaskGroup.
+   *
+   * @param {TaskGroupId} id - The unique identifier for the task group.
+   */
   constructor(id: TaskGroupId) {
     this.id = id;
     this.children = new Map();
   }
 
+  /**
+   * Adds a task or task group to the collection.
+   *
+   * @param {Task | TaskGroup} task - The task or task group to be added to this group.
+   */
   addChild(task: Task | TaskGroup) {
     this.children.set(task.id, task);
   }
 
+  /**
+   * Removes a task or task group from the collection by its identifier.
+   *
+   * @param {TaskId | TaskGroupId} taskId - The identifier of the task or task group to be removed.
+   */
   removeChild(taskId: TaskId | TaskGroupId) {
     this.children.delete(taskId);
   }
 }
 
+/**
+ * Manages and orchestrates the execution of task groups.
+ *
+ * This class extends EventEmitter to handle task execution flow control. It maintains a collection
+ * of task groups, allowing for addition, removal, execution, and retrieval of these groups.
+ * It also provides a method to serialize the state of the task groups.
+ *
+ * @extends EventEmitter
+ * @property {Map<TaskGroupId, TaskGroup>} taskGroups - A private map to manage task groups by their IDs.
+ */
 export class FlowControl extends EventEmitter {
   // Map to quickly access tasks by id
   private taskGroups: Map<TaskGroupId, TaskGroup>;
@@ -116,21 +234,53 @@ export class FlowControl extends EventEmitter {
     this.taskGroups = new Map();
   }
 
+  /**
+   * Adds a task group to the collection.
+   *
+   * @param {TaskGroup} taskGroup - The task group to be added.
+   */
+
   addGroup(taskGroup: TaskGroup) {
     this.taskGroups.set(taskGroup.id, taskGroup);
   }
 
+  /**
+   * Removes a task group from the collection using its ID.
+   *
+   * @param {TaskGroupId} taskGroupId - The ID of the task group to be removed.
+   */
   removeGroup(taskGroupId: TaskGroupId) {
     this.taskGroups.delete(taskGroupId);
   }
+
+  /**
+   * Retrieves all task groups.
+   *
+   * @returns {Map<TaskGroupId, TaskGroup>} The current map of task groups.
+   */
 
   getTaskGroups() {
     return this.taskGroups;
   }
 
+  /**
+   * Retrieves a specific task group by its ID.
+   *
+   * @param {TaskGroupId} taskGroupId - The ID of the task group to be retrieved.
+   * @returns {TaskGroup | undefined} The requested task group, or undefined if not found.
+   */
+
   getTaskGroup(taskGroupId: TaskGroupId) {
     return this.taskGroups.get(taskGroupId);
   }
+
+  /**
+   * Executes a task group and its child tasks recursively.
+   * Emits a 'success' event upon completion of a task group.
+   *
+   * @param {TaskGroup} taskGroup - The task group to be executed.
+   * @private
+   */
 
   private runTaskGroup = async (taskGroup: TaskGroup) => {
     const tasks = Array.from(taskGroup.children.values());
@@ -145,6 +295,9 @@ export class FlowControl extends EventEmitter {
     this.emit("success", `task-group-completed: ${taskGroup.id}`);
   };
 
+  /**
+   * Executes all task groups managed by the FlowControl instance.
+   */
   run = async () => {
     const taskGroups = Array.from(this.taskGroups.values());
     for (const taskGroup of taskGroups) {
@@ -152,6 +305,11 @@ export class FlowControl extends EventEmitter {
     }
   };
 
+  /**
+   * Serializes the state of all task groups into a structured format.
+   *
+   * @returns {SerializedState} The serialized state of all task groups.
+   */
   getSerializedState = (): SerializedState => {
     const serializeTaskGroup = (taskGroup: TaskGroup): SerializedState => {
       let groupState: SerializedState = {};
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,18 @@
+/**
+ * Creates a promise that resolves after a specified duration.
+ *
+ * This method can be used to introduce a delay in code execution. The promise
+ * will resolve after the specified time has elapsed, effectively creating a pause.
+ *
+ * @param {number} time - The duration in milliseconds for which the promise should wait before resolving.
+ * @returns {Promise<void>} A promise that resolves after the specified duration.
+ *
+ * @example
+ * // Using the wait function to introduce a delay
+ * wait(2000).then(() => {
+ *   console.log('2 seconds have passed');
+ * });
+ */
 export const wait = (time: number) => {
   return new Promise<void>((resolve) => {
     setTimeout(() => {
