73019b9c1bc32195fe04bf8cd3a0da6ad40b729b
DJ Petersen <thedjpetersen@gmail.com>
2023-12-16 08:16:36 -0800

    Add release version and outdated depedencies checker

--- scripts/generate-badge-data.ts
+++ scripts/generate-badge-data.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
+import process from "process";
+import { execSync } from "child_process";
 
 const coverageReport = "./coverage/coverage-summary.json"; // Adjust the path as needed
+const packageJson = "./package.json"; // Adjust the path as needed
 
 function determineColor(coverage) {
   if (coverage >= 80) {
@@ -12,15 +15,52 @@ function determineColor(coverage) {
   }
 }
 
+function writeToOutputFile(outputName, outputValue) {
+  const outputFile = process.env.GITHUB_OUTPUT;
+  if (outputFile) {
+    fs.appendFileSync(outputFile, `${outputName}=${outputValue}\n`);
+  } else {
+    console.error("GITHUB_OUTPUT environment variable not found.");
+    process.exit(1);
+  }
+}
+
+function checkNpmDependencies() {
+  try {
+    execSync("npm outdated");
+    return "up-to-date";
+  } catch {
+    return "outdated";
+  }
+}
+
+function getCurrentReleaseVersion() {
+  if (fs.existsSync(packageJson)) {
+    const packageData = JSON.parse(fs.readFileSync(packageJson, "utf8"));
+    return packageData.version || "unknown";
+  } else {
+    return "unknown";
+  }
+}
+
+// Check coverage
 if (fs.existsSync(coverageReport)) {
   const coverage = JSON.parse(fs.readFileSync(coverageReport, "utf8"));
-  const linesCoverage = coverage.total.lines.pct; // Extracting line coverage percentage
+  const linesCoverage = coverage.total.lines.pct;
   const color = determineColor(linesCoverage);
 
-  console.log(`::set-output name=coverage-label::Coverage`);
-  console.log(`::set-output name=coverage-message::${linesCoverage}%`);
-  console.log(`::set-output name=coverage-color::${color}`);
+  writeToOutputFile("coverage-label", "Coverage");
+  writeToOutputFile("coverage-message", `${linesCoverage}%`);
+  writeToOutputFile("coverage-color", color);
 } else {
   console.error("Coverage report not found.");
   process.exit(1);
 }
+
+// Check NPM dependencies
+const dependenciesStatus = checkNpmDependencies();
+writeToOutputFile("dependencies-status", dependenciesStatus);
+
+// Get current release version
+const releaseVersion = getCurrentReleaseVersion();
+writeToOutputFile("release-version", releaseVersion);
